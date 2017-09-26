e8ab678856e0fa9a2469fdbdcf41f808141d26ff
zhi.huang <371351694@qq.com>
2017-09-26 14:41:52 +0800

    测试

--- ant-deliver-service/src/main/java/com/bx/ant/controller/ApiAccountController.java
+++ ant-deliver-service/src/main/java/com/bx/ant/controller/ApiAccountController.java
@@ -1,10 +1,11 @@
 package com.bx.ant.controller;
 
-import com.alibaba.dubbo.config.annotation.Reference;
 import com.mobian.pageModel.Json;
 import com.mobian.service.MbShopServiceI;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
 
 /**
  * Created by guxin on 2017/4/22.
@@ -15,13 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/api/deliver/accountController")
 public class ApiAccountController extends BaseController {
 
-    @Reference
+    @Resource
     private MbShopServiceI mbShopService;
 
     @RequestMapping("/test")
     public Json getShop(){
         Json json = new Json();
-        json.setObj(mbShopService.getFromCache(1363));
+        json.setObj(mbShopService.getFromCache(35));
         return json;
     }
 
