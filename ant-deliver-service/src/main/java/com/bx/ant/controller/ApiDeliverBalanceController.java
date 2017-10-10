6b42d8ac11b7ea4bf05f28cef80050e435e9ef72
wanxp <977741432@qq.com>
2017-10-10 15:04:47 +0800

    change methods in deliverOrderController

--- ant-deliver-service/src/main/java/com/bx/ant/controller/ApiDeliverBalanceController.java
+++ ant-deliver-service/src/main/java/com/bx/ant/controller/ApiDeliverBalanceController.java
@@ -2,9 +2,11 @@ package com.bx.ant.controller;
 
 import com.bx.ant.service.DeliverOrderServiceI;
 import com.bx.ant.service.DeliverOrderShopPayServiceI;
+import com.mobian.absx.F;
 import com.mobian.pageModel.*;
 import com.mobian.pageModel.DeliverOrderShopPay;
 import com.mobian.service.MbBalanceLogServiceI;
+import com.mobian.service.MbBalanceServiceI;
 import com.mobian.service.MbShopServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by wanxp 2017/9/22
@@ -27,6 +35,9 @@ public class ApiDeliverBalanceController extends BaseController {
 
     @Autowired
     private DeliverOrderServiceI deliverOrderService;
+
+    @Autowired
+    private MbBalanceServiceI mbBalanceService;
 
     @Autowired
     private MbBalanceLogServiceI mbBalanceLogService;
@@ -64,15 +75,33 @@ public class ApiDeliverBalanceController extends BaseController {
 
     /**
      * 获取门店运单账号流水
-     * @param mbBalanceLog
+     * @param date
      * @param pageHelper
      * @return
      */
     @RequestMapping("/viewDeliverBanlanceLogDataGrid")
     @ResponseBody
-    public Json viewDeliverBanlanceLogDataGrid(MbBalanceLog mbBalanceLog, PageHelper pageHelper) {
+    public Json viewDeliverBanlanceLogDataGrid(String date,PageHelper pageHelper) {
         Json j = new Json();
         DataGrid dataGrid;
+        MbBalanceLog mbBalanceLog = new MbBalanceLog();
+        Calendar now = Calendar.getInstance();
+
+        if (date == null) {
+             date = now.get(Calendar.YEAR) + "-" + now.get(Calendar.MONTH) ;
+        }
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        try {
+            mbBalanceLog.setUpdatetimeBegin(dateFormat.parse(date
+                    + "-" + now.getActualMinimum(Calendar.DAY_OF_MONTH) + " 00:00:00"));
+            mbBalanceLog.setUpdatetimeEnd(dateFormat.parse(date
+                    +  "-" + now.getActualMaximum(Calendar.DAY_OF_MONTH) + " 00:00:00"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
         //TODO 测试时设置shop ID值,若真正使用从token中获取
 //        TokenWrap token = getTokenWrap(request);
 //        Integer shopId = token.getShopId();
@@ -83,5 +112,28 @@ public class ApiDeliverBalanceController extends BaseController {
         j.setMsg("u know");
         j.setSuccess(true);
         return j;
+    }
+
+    /**
+     * 获取门店运单账户余额
+     * @param request
+     * @return
+     */
+    @RequestMapping("/viewDeliverBalance")
+    @ResponseBody
+    public Json viewBalance(HttpServletRequest request) {
+        Json j = new Json();
+//        TokenWrap tokenWrap = getTokenWrap(request);
+//        Integer shopId = tokenWrap.getShopId();
+       Integer shopId = 1332;
+       if (!F.empty(shopId)) {
+           j.setObj(mbBalanceService.addOrGetMbBalanceDelivery(shopId));
+           j.setMsg("u know");
+           j.setSuccess(true);
+       } else {
+           j.setMsg("shopId不能为空");
+           j.setSuccess(false);
+       }
+       return j;
     }
 }
