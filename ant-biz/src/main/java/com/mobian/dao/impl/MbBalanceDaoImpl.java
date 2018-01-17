f8000a4f812e30a05e7a765c61bb01175d467b2c
zhi.huang <371351694@qq.com>
2018-01-17 19:53:25 +0800

    fixbug

--- ant-biz/src/main/java/com/mobian/dao/impl/MbBalanceDaoImpl.java
+++ ant-biz/src/main/java/com/mobian/dao/impl/MbBalanceDaoImpl.java
@@ -3,9 +3,20 @@ package com.mobian.dao.impl;
 import com.mobian.dao.MbBalanceDaoI;
 import com.mobian.model.TmbBalance;
 
+import org.hibernate.SQLQuery;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class MbBalanceDaoImpl extends BaseDaoImpl<TmbBalance> implements MbBalanceDaoI {
 
+    @Override
+    public Integer getAmountById(Integer balanceId) {
+        SQLQuery q = getCurrentSession().createSQLQuery("select amount from mb_balance where id =:id");
+        q.setParameter("id", balanceId);
+        return (Integer) q.uniqueResult();
+    }
 }
